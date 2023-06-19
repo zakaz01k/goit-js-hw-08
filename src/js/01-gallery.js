9a526ab214fa4fdbb066b30bc511e5adc2a3998d
denys <dedpro88@gmail.com>
2023-06-19 21:49:55 +0200

    first commit

---
+++ src/js/01-gallery.js
@@ -0,0 +1,6 @@
+import SimpleLightbox from 'simplelightbox';
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
